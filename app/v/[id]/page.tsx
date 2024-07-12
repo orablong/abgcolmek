053a6b2525c2c670a90723b99839b97c195961cd
orablong <163920831+orablong@users.noreply.github.com>
2024-07-13 02:34:58 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -137,7 +137,7 @@ export default async function Video({ params }: PageProps) {
         </section>
             <iframe
                 className="w-full h-[30vh] md:h-[55vh] lg:h-[70vh]"
-                src={`https://ds2play.com/e/${file.filecode}`}
+                src={`https://${upstream}/e/${file.filecode}`}
                 scrolling="no"
                 title={file.title}
                 frameBorder={0}
